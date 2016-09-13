68dd84b735cb5324b4240291ad95f501583a877d
hleighcurtis <hcurtis@caltech.edu>
2016-09-13 14:28:51 -0700

    updated bug

--- l20.py
+++ l20.py
@@ -13,4 +13,11 @@ def xa_to_diameter(xa):
     #A = (pi(d^2))/4
     diameter = np.sqrt(4* xa / np.pi)
 
-    return diameter 
+    return diameter
+
+A = np.array([[6.7, 1.3, 0.6, 0.7],
+a function that takes in an array of cross-sectional areas and returns an array of diameters.                 [0.1, 5.5, 0.4, 2.4],
+                [0.0, 1.5, 3.4, 7.5],
+                [1.1, 0.8, 4.5, 1.7]])
+
+b = np.array([1.1, 2.3, 3.3, 3.9])
